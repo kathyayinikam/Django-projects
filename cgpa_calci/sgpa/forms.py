3458cbe066a4f5c3e864c8bd5a54804a41ec4bab
kathyayinikam <145150034+kathyayinikam@users.noreply.github.com>
2024-05-08 21:16:31 +0530

    Add files via upload

--- cgpa_calci/sgpa/forms.py
+++ cgpa_calci/sgpa/forms.py
@@ -1,4 +1,17 @@
 from django import forms 
+
 class inputform(forms.Form):
-    name=forms.CharField(max_length=10)
-    input=forms.IntegerField(min_value=1, max_value=7, label="$3")
+    input1=forms.FloatField(min_value=1, max_value=10, label="1st sem SGPA")
+    input2=forms.FloatField(min_value=1, max_value=10, label="2nd sem SGPA")
+    input3=forms.FloatField(min_value=1, max_value=10, label="3rd sem SGPA")
+    input4=forms.FloatField(min_value=1, max_value=10, label="4th sem SGPA")
+    input5=forms.FloatField(min_value=1, max_value=10, label="5th sem SGPA")
+
+class inputform2(forms.Form):  
+    input6=forms.FloatField(min_value=1, max_value=100, label="ATC(21CS51)")
+    input7=forms.FloatField(min_value=1, max_value=100, label="CN(21CS52)")
+    input8=forms.FloatField(min_value=1, max_value=100, label="DBMS(21CS53)")
+    input9=forms.FloatField(min_value=1, max_value=100, label="AIML(21CS54)")
+    input10=forms.FloatField(min_value=1, max_value=100, label="RMIPR(21CS55)")
+    input11=forms.FloatField(min_value=1, max_value=100, label="CIV(21CS56)")
+    input12=forms.FloatField(min_value=1, max_value=100, label="C#(21CSL582)")
--- cgpa_calci/sgpa/views.py
+++ cgpa_calci/sgpa/views.py
@@ -1,20 +1,39 @@
 from django.shortcuts import render
-from sgpa.forms import inputform
+from sgpa.forms import inputform, inputform2
 
 def home(request):
     if request.method == "POST":
         form1 = inputform(request.POST)
-        if form1.is_valid():
-            data = form1.cleaned_data
-            n1 = data.get("input")
-            result = fact(n1)
-            return render(request, "index.html", {'param1': result, 'param2': n1, 'form': form1})
+        form2 = inputform2(request.POST)
+        #result1=0
+        result2=0
+       # if form1.is_valid():
+           # data = form1.cleaned_data
+            #n1 = data.get("input1")
+            #n2 = data.get("input2")
+            #n3 = data.get("input3")
+           # n4 = data.get("input4")
+            #n5 = data.get("input5")
+           # result1 = round((n1 + n2 + n3 + n4 + n5) / 5, 2)
+        if form2.is_valid():
+            data = form2.cleaned_data
+            n6 = data.get("input6")
+            n7 = data.get("input7")
+            n8 = data.get("input8")
+            n9 = data.get("input9")
+            n10 = data.get("input10")
+            n11 = data.get("input11")
+            n12 = data.get("input12")
+            point1 = 30 if n6 == 100 else ((n6 // 10) + 1) * 3
+            point2 = 40 if n7 == 100 else ((n7 // 10) + 1) * 4
+            point3 = 40 if n8 == 100 else ((n8 // 10) + 1) * 4
+            point4 = 30 if n9 == 100 else ((n9 // 10) + 1) * 3
+            point5 = 20 if n10 == 100 else ((n10 // 10) + 1) * 2
+            point6 = 10 if n11 == 100 else ((n11 // 10) + 1) * 1
+            point7 = 10 if n12 == 100 else ((n12 // 10) + 1) * 1
+            result2 = round(((point1 + point2 + point3 + point4 + point5 + point6 + point7) / 18), 2)
+            return render(request, "index.html", {  'param2': result2, 'form2': form2})
     else:
-        form1 = inputform()  
-    return render(request, "index.html", {'form': form1})
-
-def fact(n1):
-    result = 1
-    for i in range(1, n1 + 1):
-        result = result * i
-    return result
+     
+        form2 = inputform2()
+    return render(request, "index.html", { 'form2': form2})
